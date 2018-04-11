021bd7eeb0417d6f40d15225ecef0e2cb6e52ced
Nathaniel Corley <nscorley@gmail.com>
2018-04-11 00:34:06 -0400

    gh-pages branch

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App';
-import './sass/base.css';
-import './sass/chessboard-0.3.0.min.css';
-
-ReactDOM.render(<App />, document.getElementById('root'));
